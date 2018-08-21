7d96bdcd59ce5af7dd430d1fedf6400de6d82318
Jan Stránský <jan.stransky@arnal.cz>
2018-08-21 08:07:04 +0200

    Change routing prefix

--- src/index.js
+++ src/index.js
@@ -552,6 +552,7 @@ const Project = {
 }
 
 const root = document.getElementById('data-explorer')
+m.route.prefix('')
 m.route(root, '/', {
   '/': { render: () => m(Layout, m(ProjectList)) },
   '/project/:id': { render: (vnode) => m(Layout, m(Project, vnode.attrs)) }
